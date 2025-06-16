ca05390be41e68337717fad9031ce981b4abc768
sneha gupta <snehagupta98930@gmail.com>
2025-06-16 18:14:06 +0530

    fix

--- packages/app/src/hooks/useChainRace.ts
+++ packages/app/src/hooks/useChainRace.ts
@@ -2238,7 +2238,7 @@ export function useChainRace() {
 
 
               await provider.waitForTransaction(transaction_hash, {
-                      retryInterval: 150,
+                      retryInterval: 200,
                 successStates: ['ACCEPTED_ON_L2']
               });
 
